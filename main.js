89e5a9efb85be2ca672db769c04af39c9e972e75
Max Haider <max_haiderw7@hotmail.com>
2023-01-14 14:26:53 -0300

    firstCommit

---
+++ main.js
@@ -0,0 +1,13 @@
+let PrimeiroNumero = document.querySelector("#CampoA");
+let SegundoNumero = document.querySelector("#CampoB");
+
+const enviar = document.querySelector("#conferir");
+
+enviar.addEventListener("click", function(){
+
+    if(PrimeiroNumero.value >= SegundoNumero.value){
+        alert("Inválido, para proseguir digite um valor maior no segundo campo");
+    }   else{
+        alert("Pode prosseguir");
+    }
+})
